82d6143df246b4e6714d05e6bdedaadfa315e7f7
user <user@tejaswini>
2018-06-25 12:20:19 +0530

    my first commit

---
+++ src/Generic/BaseTest.java
@@ -0,0 +1,11 @@
+package Generic;
+import.org.selenium.firefox.FirefoxDriver;
+public class BaseTest implements Constant {
+	P
+	public  void openapp()
+	{
+		System.setProperty(GECKO_KEY,GECKO_VALUE);
+	 driver=new FirefoxDriver();
+	}
+
+}
---
+++ src/Generic/Constant.java
@@ -0,0 +1,6 @@
+package Generic;
+
+public interface Constant {
+String	GECKO_KEY="webdriver.gecko.driver";
+String GECKO_VALUE="./jar/geckodriver.exe";
+}
